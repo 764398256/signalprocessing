8d1e62f232bb324da58c483ad67a67f10cd27ad8
congfu <congfu@mobvoi.com>
2018-10-21 19:39:26 +0800

    Update the time domain aec.

--- matlab/aec/AecTime.m
+++ matlab/aec/AecTime.m
@@ -1,17 +1,14 @@
-clear all;
-close all;
-
-nu_nlms = 0.8;
+clear all;clc;close all;
 
 testcase = 2;
 
 if testcase == 1
     micInFile = 'data/noise_mic20.wav';
-    spkInFile = 'data/noise_spk.wav';
+    spkInFile = 'data/noise_mic20.wav';
     micOutFile = 'data/out.wav';
 elseif testcase == 2
     micInFile = 'data/micin_16k.wav';
-    spkInFile = 'data/spkin_16k.wav';
+    spkInFile = 'data/micin_16k.wav';
     micOutFile = 'data/out.wav';
 end;
 
@@ -28,27 +25,47 @@ end;
 assert(mic_fs == spk_fs);      % does not support re-sampling
 fs = mic_fs;
 
-% 100 ms data
-nTaps = ceil(10 * 10 * fs / 1000);
+r = Rir(mic_fs, 0.1);
+mic = conv(mic, r);
+
+% Configs
+nTaps = length(r);
+nu_nlms = 0.8;
 
 nSamples = min(size(spk, 1), size(mic, 1));
-
 h = zeros(nTaps, 1);
 clean = zeros(nSamples, 1);
-
 for ii = nTaps:nSamples
     x = spk(ii-nTaps+1:ii);
     e = mic(ii, 1) - h.'*x;
     power = x.'*x;
-    if (power/nTaps > 0.032^2)
+    % if (power/nTaps > 0.032^2)
         h = h + nu_nlms*(e*x./power);
-    end;
+    % end;
     clean(ii) = e;
 end;
 
-if(0)
-figure(2);
+frameSize = fs*10/1000;
+windowSize = frameSize*2;
+
+input2 = buffer(mic, windowSize, windowSize-frameSize, 'nodelay');
+output2 = buffer(clean, windowSize, windowSize-frameSize, 'nodelay');
+
+N2 = min([size(input2,2) size(output2,2)]);
+input2 = input2(:, 1:N2);
+output2 = output2(:, 1:N2);
+
+erleSorted = sort(10*log10(var(input2)./var(output2)));
+e95p = erleSorted(round(length(erleSorted)*0.95));
+
+fprintf('erle is %f\n', e95p);
+
+figure(90);
+plot(erleSorted);
+grid on;
+
+figure(91);
 plot(clean, 'b');
-end;
+grid on;
 
 audiowrite(micOutFile, clean, fs);
